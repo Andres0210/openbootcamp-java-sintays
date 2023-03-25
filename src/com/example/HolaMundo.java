463b45902a68f6325eb02fb9b7358fd6218e610c
Andres0210 <ing_amilkar@hotmail.com>
2023-03-24 20:25:15 -0500

    Add the activity to repository

---
+++ src/com/example/HolaMundo.java
@@ -0,0 +1,25 @@
+package com.example;
+
+public class HolaMundo {
+
+    public static void main(String[] args) {
+        byte number1 = 1;
+        short number2 = 20;
+        int number3 = 300;
+        long number4 = 50000;
+        float number5 = 1.0f;
+        double number6 = 1.0;
+        char character = 'a';
+        String string = "Hello I'm a string";
+        System.out.println("Hola mundo desde Valledupar");
+        System.out.println(number1);
+        System.out.println(number2);
+        System.out.println(number3);
+        System.out.println(number4);
+        System.out.println(number5);
+        System.out.println(number6);
+        System.out.println(character);
+        System.out.println(string);
+    }
+
+}
